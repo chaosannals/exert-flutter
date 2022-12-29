e6eab56ec5539dfe63b0a8784cdfb3adfb31981d
chaosannals <chaosannals@outlook.com>
2022-12-29 09:07:56 +0800

    save

--- sshdesktop/lib/pages/ssh/list_page.dart
+++ sshdesktop/lib/pages/ssh/list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:sshdesktop/widgets/nav_scaffold.dart';
 
 class SshListPage extends StatefulWidget {
@@ -9,12 +11,31 @@ class SshListPage extends StatefulWidget {
 }
 
 class _SshListPageState extends State<SshListPage> {
+  final searchTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return NavScaffold(
       child: Column(
-        children: const [
-          Text("SSH"),
+        children: [
+          Row(
+            children: [
+              CupertinoButton(
+                onPressed: () {},
+                child: const Text("添加"),
+              ),
+              SizedBox(
+                width: 100.w,
+                child: CupertinoTextField(
+                  controller: searchTextController,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'[0-9A-Za-z]'))
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const Text("SSH"),
         ],
       ),
     );
